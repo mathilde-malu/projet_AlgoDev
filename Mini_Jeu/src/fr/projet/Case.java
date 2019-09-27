e86a366e0f37ed03e65267954ae699f76f09339a
MHMau <MHMau@DESKTOP-RAM4SCN.home>
2019-09-27 22:18:10 +0200

    Attribut + setter/getter Classe fr.projet.Case

--- Mini_Jeu/src/fr/projet/Case.java
+++ Mini_Jeu/src/fr/projet/Case.java
@@ -1,5 +1,30 @@
 package fr.projet;
 
 public class Case {
+	 private char val;
+	 private boolean modifiable;
 
-}
+	 public Case() {
+		  setVal('0');
+		  setModifiable(true); }
+
+	public Case(char val, boolean modifiable) {
+		  this.setVal(val);
+		  this.setModifiable(modifiable); }
+
+	public char getVal() {
+		return val;
+	}
+
+	public void setVal(char val) {
+		this.val = val;
+	}
+
+	public boolean isModifiable() {
+		return modifiable;
+	}
+
+	public void setModifiable(boolean modifiable) {
+		this.modifiable = modifiable;
+	}
+		}
